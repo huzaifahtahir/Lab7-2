e3ac231ca5227289c2e0512be4fb5d85d3dd6853
Huzaifah Tahir <tahirhuzaifah@outlook.com>
2018-12-14 03:10:56 -0500

    TestChange

--- Lab7-2/Lab7-2-HuzaifahTahir.cpp
+++ Lab7-2/Lab7-2-HuzaifahTahir.cpp
@@ -5,5 +5,29 @@ using namespace std;
 
 int main() {
 
+	const int SIZE = 10;
+
+	int theArray[SIZE];
+
+
+
+	// INPUT: save the scores into theArray.
+
+	//...
+
+
+
+	// PROCESS: count the perfect scores (i.e., 100s) in the array
+
+	//...
+
+	// OUTPUT:  display the count of prefect scores on the screen
+
+	//...
+
+
+
+	return 0;
+
 }
 
