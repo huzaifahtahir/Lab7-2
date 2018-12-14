acf7458a09b82c6af7c2ba78c6c3311128c631ac
Huzaifah Tahir <tahirhuzaifah@outlook.com>
2018-12-14 03:44:46 -0500

    Test

--- Lab7-2/Lab7-2-HuzaifahTahir.cpp
+++ Lab7-2/Lab7-2-HuzaifahTahir.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int main() {
 
+	system("title Lab7-2-HuzaifahTahir");
+
 	const int SIZE = 10;
 
 	int theArray[SIZE];
@@ -16,14 +18,31 @@ int main() {
 	//...
 
 
+	for (int i = 0; i < SIZE; i++) {
+		
+		cout << "Enter score: ";
+		cin >> theArray[i];
+
+	}
+
+
 
 	// PROCESS: count the perfect scores (i.e., 100s) in the array
 
 	//...
 
+	int counter = 0;
+	for (int i = 0; i < SIZE; i++) {
+		if (theArray[i] == 100)
+			counter++;
+	}
+
 	// OUTPUT:  display the count of prefect scores on the screen
 
 	//...
+
+	cout << "The number of perfect scores in the array: " << counter << ".\n";
+	system("pause");
 
 
 
